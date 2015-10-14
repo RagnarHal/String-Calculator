2d4e6ff17b8f1c02e4e93985de6ff73368c022d0
Ragnar <ragnarmh13@ru.is>
2015-10-14 12:27:40 +0000

    Refactoring

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,6 +3,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 //Remember to refactor after each passing test.
 public class Calculator {
+	final static String delim = ",";
 	//This is the only function that is being tested.
 
 
@@ -11,20 +12,26 @@ public class Calculator {
 			return 0;
 		}
 
-		String[] tokens = text.split(",");
+		return sum(splitString(text));
+	}
 
+
+	/* Helpers */
+	private static int toInt(String num) {
+		return Integer.parseInt(num);
+	}
+
+	private static int sum(String[] nums) {
 		int sum = 0;
 
-		for(String number : tokens) {
+		for(String number : nums) {
 			sum += toInt(number);
 		}
 
 		return sum;
 	}
 
-
-/* Helpers */
-	private static int toInt(String num) {
-		return Integer.parseInt(num);
+	private static String[] splitString(String text) {
+		return text.split(delim);
 	}
 }
